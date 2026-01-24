2f58094869b3165a01c5cb79c2d6ea7adb97b3b6
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2026-01-24 11:22:15 +0530

    seperation in css styling

--- ffmpeg_blog/settings.py
+++ ffmpeg_blog/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 LOGIN_REDIRECT_URL = 'command_list'
 LOGOUT_REDIRECT_URL = 'command_list'
